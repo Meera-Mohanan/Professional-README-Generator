aafc87ce7dc745a81136a35a3b004fe2897af3dc
Meera-Mohanan <126405858+Meera-Mohanan@users.noreply.github.com>
2023-05-23 21:55:40 +0800

    generateMarkdown.js updated

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -5,10 +5,18 @@ function renderLicenseBadge(license) {
   }
   return renderLicenseSection(license)
 }
+
 // retrieves url to license badge
 function renderLicenseLink(license) {
   return license ? `https://img.shields.io/badge/license-${license}-blue` : '';
 }
+
+// formats license bade url into README section
+function renderLicenseSection(license) {
+  licenseLink = renderLicenseLink(license);
+  return license ? `![${license} license](${licenseLink})` : '';
+}
+
 // takes data from user response and formats into a README format
 function generateMarkdown(data) {
   licenseBadge = renderLicenseBadge(data.license);
