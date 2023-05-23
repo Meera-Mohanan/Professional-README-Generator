39b42af02ed4fb06e008f24de916414ad4bfe4b9
Meera-Mohanan <126405858+Meera-Mohanan@users.noreply.github.com>
2023-05-23 21:40:40 +0800

    generateMarkdown.js updated

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,19 +1,53 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
-
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
-
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
-
-// TODO: Create a function to generate markdown for README
+// renders license badge
+function renderLicenseBadge(license) {
+  if (!license){
+    return '';
+  }
+  return  renderLicenseSection(license)
+}
+// retrieves url to license badge
+function renderLicenseLink(license) {
+  return license ? `https://img.shields.io/badge/license-${license}-blue`: '';
+}
+// takes data from user response and formats into a README format
 function generateMarkdown(data) {
+  licenseBadge = renderLicenseBadge(data.license);
   return `# ${data.title}
+  ${licenseBadge}
+  ## Description 
+  
+  \n${data.description}\n
 
+  ## Table of Contents
+  * [installation](#installation)
+  * [Usage](#usage)
+  * [License](#license)
+  * [Contributing](#contributing)
+  * [Tests](#tests)
+  * [Questions](#questions)
+  
+  ## Installation
+  To install necessary dependencies, run the following command:
+  \`\`\`
+  ${data.install}
+  \`\`\`
+  ## Usage
+  \n${data.usage}\n
+
+  ## License
+  This project is licensed under the ${data.license}.
+
+  ## Contributing
+  \n${data.contribution}\n
+
+  ## Tests
+  To run test, run the following command:
+  \`\`\`
+  ${data.tests}
+  \`\`\`
+
+  ## Questions
+ \n If you have any questions about the repo, open an issue or contact me directly at ${data.email}. you can find more of my work at [${data.username}](https://github.com/${data.username})\n
 `;
 }
 
